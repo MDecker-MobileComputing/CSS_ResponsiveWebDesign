e1ead5741d1b63a2ce15ab688796a66e30f029c5
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-09-25 16:53:17 +0200

    Color Change: Dynamically show viewport width

--- docs/ColorChange/viewport-width.js
+++ docs/ColorChange/viewport-width.js
@@ -1,18 +1,32 @@
 "use strict";
 
+/**
+ * Just include this script in your HTML file to display the current viewport width in a paragraph element.
+ *
+ * Include it using the following code in your HTML file's <head> section:
+ * <script src="viewport-width.js" defer></script>
+ *
+ * Attribute "defer" is important to ensure that the script runs after the
+ * HTML document has been fully parsed.
+ */
+
+
 let paragraphViewportWidth = null;
 
 
+/**
+ * Creates a paragraph element to display the current viewport width and adds it to the document body.
+ * Also sets up an event listener for window resize events to update the displayed viewport width.
+ */
 window.addEventListener( "load", function () {
 
-    paragraphViewportWidth = document.getElementById( "paragraph-viewport-width" );
-    if ( paragraphViewportWidth === null ) {
+    paragraphViewportWidth = document.createElement( "p" );
 
-        console.error( "Paragraph element not found!" );
-        return;
-    }
+    document.body.appendChild( paragraphViewportWidth );
 
     window.addEventListener( "resize", showViewportWidth );
+
+    showViewportWidth();
 });
 
 
@@ -22,7 +36,7 @@ window.addEventListener( "load", function () {
 function showViewportWidth() {
 
     const viewportWidth = window.innerWidth;
-    console.log( `Viewport width: ${viewportWidth}px` );
 
-    paragraphViewportWidth.textContent = `Viewport width: ${viewportWidth}px`;
+    paragraphViewportWidth.textContent =
+            `Current viewport width: ${viewportWidth}px`;
 }
